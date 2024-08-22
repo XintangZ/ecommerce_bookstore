61113f396caf77ac31c48378622a64f5615d2ca5
Xintang ZHANG <xtzhang1994@gmail.com>
2024-08-22 17:39:34 -0400

    modified Book model modified Book endpoints added authentication to Book routes

--- server/src/routes/book.route.ts
+++ server/src/routes/book.route.ts
@@ -1,18 +1,19 @@
 import { Router } from 'express';
-import { createBook, deleteBook, getAllBooks, getBook, updateBook } from '../controllers';
+import { createBook, deleteBook, getAllBooks, getBookById, updateBook } from '../controllers';
+import { authenticateJWT } from '../middlewares';
 
 export const bookRoute = () => {
 	const router = Router();
 
 	router.get('/books', getAllBooks);
 
-	router.get('/books/:id', getBook);
+	router.get('/books/:id', getBookById);
 
-	router.post('/books/add', createBook);
+	router.post('/books', authenticateJWT, createBook);
 
-	router.put('/books/update/:id', updateBook);
+	router.put('/books/:id', authenticateJWT, updateBook);
 
-	router.delete('/books/delete/:id', deleteBook);
+	router.delete('/books/:id', authenticateJWT, deleteBook);
 
 	return router;
 };
