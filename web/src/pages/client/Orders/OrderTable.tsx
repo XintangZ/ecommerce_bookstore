a385cc105a94fd2e1a8324749173a1cf453477b6
carolyn <carolyntie1204@hotmail.com>
2024-08-30 13:18:44 -0400

    no order situation

--- web/src/pages/client/Orders/OrderTable.tsx
+++ web/src/pages/client/Orders/OrderTable.tsx
@@ -15,6 +15,7 @@ import {
     DialogContent,
     DialogContentText,
     DialogTitle,
+    Typography,
 } from '@mui/material';
 import { KeyboardArrowDown, KeyboardArrowUp } from '@mui/icons-material';
 import { useUpdateOrder, useUpdateMultipleBookStocks } from '../../../services';
@@ -106,89 +107,95 @@ const OrderTable: React.FC<OrderTableProps> = ({ orders, setOrders, open, setOpe
 
     return (
         <>
-            <TableContainer>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell />
-                            <TableCell>Order ID</TableCell>
-                            <TableCell>Total Amount</TableCell>
-                            <TableCell>Placed At</TableCell>
-                            <TableCell>Status</TableCell>
-                            <TableCell>Shipping Address</TableCell>
-                            <TableCell>Action</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {orders.map(order => (
-                            <React.Fragment key={order._id}>
-                                <TableRow>
-                                    <TableCell>
-                                        <IconButton
-                                            size="small"
-                                            onClick={() => setOpen(open === order._id ? null : order._id)}
-                                        >
-                                            {open === order._id ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
-                                        </IconButton>
-                                    </TableCell>
-                                    <TableCell>{order._id}</TableCell>
-                                    <TableCell>${order.totalAmount.toFixed(2)}</TableCell>
-                                    <TableCell>{order.placedAt ? new Date(order.placedAt).toLocaleString() : 'N/A'}</TableCell>
-                                    <TableCell>{order.status}</TableCell>
-                                    <TableCell>{formatShippingAddress(order.shippingAddress)}</TableCell>
-                                    <TableCell>
-                                        {order.status === 'Pending' && (
-                                            <>
-                                                <Button
-                                                    variant="outlined"
-                                                    color="error"
-                                                    onClick={() => handleCancelClick(order._id)}
-                                                >
-                                                    Cancel
-                                                </Button>
-                                                {isAdmin && (
+            {orders.length === 0 ? (
+                <Typography variant="h6" align="center" sx={{ my: 4 }}>
+                    You haven't placed any order!
+                </Typography>
+            ) : (
+                <TableContainer>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell />
+                                <TableCell>Order ID</TableCell>
+                                <TableCell>Total Amount</TableCell>
+                                <TableCell>Placed At</TableCell>
+                                <TableCell>Status</TableCell>
+                                <TableCell>Shipping Address</TableCell>
+                                <TableCell>Action</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {orders.map(order => (
+                                <React.Fragment key={order._id}>
+                                    <TableRow>
+                                        <TableCell>
+                                            <IconButton
+                                                size="small"
+                                                onClick={() => setOpen(open === order._id ? null : order._id)}
+                                            >
+                                                {open === order._id ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
+                                            </IconButton>
+                                        </TableCell>
+                                        <TableCell>{order._id}</TableCell>
+                                        <TableCell>${order.totalAmount.toFixed(2)}</TableCell>
+                                        <TableCell>{order.placedAt ? new Date(order.placedAt).toLocaleString() : 'N/A'}</TableCell>
+                                        <TableCell>{order.status}</TableCell>
+                                        <TableCell>{formatShippingAddress(order.shippingAddress)}</TableCell>
+                                        <TableCell>
+                                            {order.status === 'Pending' && (
+                                                <>
                                                     <Button
                                                         variant="outlined"
-                                                        color="primary"
-                                                        onClick={() => handleShipClick(order._id)}
-                                                        style={{ marginLeft: '10px' }}
+                                                        color="error"
+                                                        onClick={() => handleCancelClick(order._id)}
                                                     >
-                                                        Ship
+                                                        Cancel
                                                     </Button>
-                                                )}
-                                            </>
-                                        )}
-                                    </TableCell>
-                                </TableRow>
-                                <TableRow>
-                                    <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={7}>
-                                        <Collapse in={open === order._id} timeout="auto" unmountOnExit>
-                                            <Table size="small" aria-label="order details">
-                                                <TableHead>
-                                                    <TableRow>
-                                                        <TableCell>Book ID</TableCell>
-                                                        <TableCell>Quantity</TableCell>
-                                                        <TableCell>Price</TableCell>
-                                                    </TableRow>
-                                                </TableHead>
-                                                <TableBody>
-                                                    {order.books.map(book => (
-                                                        <TableRow key={book.bookId}>
-                                                            <TableCell>{book.bookId}</TableCell>
-                                                            <TableCell>{book.quantity}</TableCell>
-                                                            <TableCell>${book.price.toFixed(2)}</TableCell>
+                                                    {isAdmin && (
+                                                        <Button
+                                                            variant="outlined"
+                                                            color="primary"
+                                                            onClick={() => handleShipClick(order._id)}
+                                                            style={{ marginLeft: '10px' }}
+                                                        >
+                                                            Ship
+                                                        </Button>
+                                                    )}
+                                                </>
+                                            )}
+                                        </TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={7}>
+                                            <Collapse in={open === order._id} timeout="auto" unmountOnExit>
+                                                <Table size="small" aria-label="order details">
+                                                    <TableHead>
+                                                        <TableRow>
+                                                            <TableCell>Book ID</TableCell>
+                                                            <TableCell>Quantity</TableCell>
+                                                            <TableCell>Price</TableCell>
                                                         </TableRow>
-                                                    ))}
-                                                </TableBody>
-                                            </Table>
-                                        </Collapse>
-                                    </TableCell>
-                                </TableRow>
-                            </React.Fragment>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+                                                    </TableHead>
+                                                    <TableBody>
+                                                        {order.books.map(book => (
+                                                            <TableRow key={book.bookId}>
+                                                                <TableCell>{book.bookId}</TableCell>
+                                                                <TableCell>{book.quantity}</TableCell>
+                                                                <TableCell>${book.price.toFixed(2)}</TableCell>
+                                                            </TableRow>
+                                                        ))}
+                                                    </TableBody>
+                                                </Table>
+                                            </Collapse>
+                                        </TableCell>
+                                    </TableRow>
+                                </React.Fragment>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            )}
 
             {/* Confirmation Dialog */}
             <Dialog open={openDialog} onClose={() => handleDialogClose(false)}>
