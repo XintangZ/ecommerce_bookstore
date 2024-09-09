423bcc9c79622c4fc048e340b318996fa0d9bb7a
aqizzz <30887282+aqizzz@users.noreply.github.com>
2024-09-09 17:19:38 -0400

    Revert "Fix cors issue"

--- server/src/middlewares/jwt.middleware.ts
+++ server/src/middlewares/jwt.middleware.ts
@@ -2,36 +2,35 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
 const authenticateJWT = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.headers.authorization?.split(' ')[1];
+    const token = req.headers.authorization?.split(' ')[1]; 
 
-  if (!token) {
-    return next(new Error('Access token is missing or invalid.'));
-  }
+    if (!token) {
+        return res.status(401).json({ message: 'Access token is missing or invalid.' });
+    }
 
-  try {
-    const secretKey = process.env.JWT_SECRET || 'default_secret_key';
-    const decoded = jwt.verify(token, secretKey);
-    res.locals.user = decoded;
+    try {
+        const secretKey = process.env.JWT_SECRET || 'default_secret_key'; 
+        const decoded = jwt.verify(token, secretKey);
+        res.locals.user = decoded; 
 
-    next();
-  } catch (error) {
-    next(new Error('Invalid or expired token.'));
-  }
+        next();
+    } catch (error) {
+        return res.status(403).json({ message: 'Invalid or expired token.' });
+    }
 };
 
 const extractUserFromJWT = (req: Request, res: Response, next: NextFunction) => {
-  const authHeader = req.headers?.authorization;
-  if (authHeader) {
-    try {
-      const token = authHeader.split(' ')[1];
-      const secretKey = process.env.JWT_SECRET || 'default_secret_key';
-      const decoded = jwt.verify(token, secretKey);
-      res.locals.user = decoded;
-    } catch (error) {
-      console.error('Token verification failed:', error);
+    const authHeader = req.headers?.authorization;
+    if (authHeader) {
+      const token = authHeader.split(' ')[1]; 
+      const secretKey = process.env.JWT_SECRET || 'default_secret_key'; 
+        const decoded = jwt.verify(token, secretKey);
+        res.locals.user = decoded; 
+        next();
+    } else {
+      // No token, but continue to the route
+      next();
     }
-  }
-  next();
-};
+  };
 
 export { authenticateJWT, extractUserFromJWT };
